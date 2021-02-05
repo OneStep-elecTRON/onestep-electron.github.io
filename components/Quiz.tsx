067bc67af84aeeb3fa9c1a0e41be2f620600e952
Smit Barmase <smitbarmase@outlook.com>
2021-02-06 01:45:56 +0530

    Quiz local updates.

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
-import axios from "../utils/api";
+import React, { useContext, useState } from "react";
 import clsx from "clsx";
-import { getCookie } from "../utils/cookie";
+import update from "immutability-helper";
 
+import axios from "../utils/api";
 import styles from "../src/css/quiz.module.css";
 
 // Global Store
@@ -37,7 +37,7 @@ export const AnswerPanel: React.FC<AnswerPanelProps> = ({
   ...props
 }) => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
-  const [userData] = useContext(GlobalContext);
+  const [userData, setUserData] = useContext(GlobalContext);
 
   const handleOnClick = (index: number) => {
     if (selectedIndex === -1) {
@@ -47,8 +47,8 @@ export const AnswerPanel: React.FC<AnswerPanelProps> = ({
         // Send the response to server
         axios
           .patch("/quiz", {
-            track: "basic",
-            isCorrect: true,
+            track: track,
+            isCorrect: correctIndex === selectedIndex,
           })
           .then(({ data }) => {
             console.log(data);
@@ -56,6 +56,50 @@ export const AnswerPanel: React.FC<AnswerPanelProps> = ({
           .catch((err) => {
             console.log(err);
           });
+
+        const increment = correctIndex === selectedIndex ? 1 : 0;
+        // Local updates!
+        if (track === "basic") {
+          const newUserData = update(userData, {
+            track: {
+              basic: {
+                quizScore: { $set: userData.track.basic.quizScore + increment },
+                totalQuizAnswered: {
+                  $set: userData.track.basic.totalQuizAnswered + 1,
+                },
+              },
+            },
+          });
+          setUserData(newUserData);
+        } else if (track === "intermediate") {
+          const newUserData = update(userData, {
+            track: {
+              intermediate: {
+                quizScore: {
+                  $set: userData.track.intermediate.quizScore + increment,
+                },
+                totalQuizAnswered: {
+                  $set: userData.track.intermediate.totalQuizAnswered + 1,
+                },
+              },
+            },
+          });
+          setUserData(newUserData);
+        } else if (track === "advanced") {
+          const newUserData = update(userData, {
+            track: {
+              advanced: {
+                quizScore: {
+                  $set: userData.track.advanced.quizScore + increment,
+                },
+                totalQuizAnswered: {
+                  $set: userData.track.advanced.totalQuizAnswered + 1,
+                },
+              },
+            },
+          });
+          setUserData(newUserData);
+        }
       }
     }
   };
