5d9225b0b27c993c803e198bb7eba6f30c4c7c3c
Smit Barmase <smitbarmase@outlook.com>
2021-02-06 16:10:09 +0530

    Fix

--- sidebars.js
+++ sidebars.js
@@ -1,15 +1,32 @@
 module.exports = {
   learningTracksSidebar: {
-    'Introduction': ['machine-learning-tracks-introduction'],
-    'PyData Tutorials': ['PyDataTutorials/numpy', 'PyDataTutorials/pandas', 'PyDataTutorials/matplotlib', 'PyDataTutorials/seaborn'],
-    'Easy Track': ['EasyTrack/easy-track-intro', 'EasyTrack/linear-regression', 'EasyTrack/logistic-regression', 'EasyTrack/decision-tree', 'EasyTrack/knn','EasyTrack/random-forest', 'EasyTrack/svm', 'EasyTrack/k-means'],
-    'Intermediate Track': ['IntermediateTrack/intermediate-track-intro'],
-    'Advanced Track': ['AdvancedTrack/advanced-track-intro']
+    Introduction: ["machine-learning-tracks-introduction"],
+    "PyData Tutorials": [
+      "PyDataTutorials/numpy",
+      "PyDataTutorials/pandas",
+      "PyDataTutorials/matplotlib",
+      "PyDataTutorials/seaborn",
+    ],
+    "Easy Track": [
+      "EasyTrack/easy-track-intro",
+      "EasyTrack/linear-regression",
+      "EasyTrack/logistic-regression",
+      "EasyTrack/decision-tree",
+      "EasyTrack/knn",
+      "EasyTrack/random-forest",
+      "EasyTrack/svm",
+      "EasyTrack/k-means",
+    ],
+    "Intermediate Track": ["IntermediateTrack/intermediate-track-intro"],
+    "Advanced Track": ["AdvancedTrack/advanced-track-intro"],
   },
   pythonCrashCourseSidebar: {
-    'Introduction': ['python-crash-course-introduction' 'PythonCrashCourse/ide.md'],
-    'Part 1': ['PythonCrashCourse/part1'],
-    'Part 2': ['PythonCrashCourse/part2'],
-    'Part 3': ['PythonCrashCourse/part3']
-  }
+    Introduction: [
+      "python-crash-course-introduction",
+      "PythonCrashCourse/ide.md",
+    ],
+    "Part 1": ["PythonCrashCourse/part1"],
+    "Part 2": ["PythonCrashCourse/part2"],
+    "Part 3": ["PythonCrashCourse/part3"],
+  },
 };
